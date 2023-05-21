51ebb21871833843932275c36f1808fe35d65afe
Daham-Mustaf <s7damust@uni-bonn.de>
2023-05-21 13:46:08 +0200

    unpack dic

--- main.py
+++ main.py
@@ -42,26 +42,37 @@ def create_an_item(item:Item):
     if db_item is not None:
         raise HTTPException(status_code=400,detail="Item already exists")
     new_item=models.Item(
-        name=item.name,
-        price=item.price,
-        description=item.description,
-        on_offer=item.on_offer
+        # name=item.name,
+        # price=item.price,
+        # description=item.description,
+        # on_offer=item.on_offer
+        **item.dict()
     )
-    
+
     db.add(new_item)
     db.commit()
+    db.refresh(new_item)
 
     return new_item
 
 @app.put('/item/{item_id}',response_model=Item,status_code=status.HTTP_200_OK)
-def update_an_item(item_id:int,item:Item):
-    item_to_update=db.query(models.Item).filter(models.Item.id==item_id).first()
-    item_to_update.name=item.name
-    item_to_update.price=item.price
-    item_to_update.description=item.description
-    item_to_update.on_offer=item.on_offer
+def update_an_item(item_id:int, item:Item):
+    # item_to_update=db.query(models.Item).filter(models.Item.id==item_id).first()
+    # item_to_update.name=item.name
+    # item_to_update.price=item.price
+    # item_to_update.description=item.description
+    # item_to_update.on_offer=item.on_offer
+    # db.commit()
+    # return item_to_update
+    item_query = db.query(models.Item).filter(models.Item.id == item_id)
+    item_db = item_query.first()
+    if item_db == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                           detail=f" Item with id:{item_id} does not exist!")
+    
+    item_query.update(item.dict(), synchronize_session=False)      
     db.commit()
-    return item_to_update
+    return {"Data": item_query.first()}
 
 @app.delete('/item/{item_id}')
 def delete_item(item_id:int):
